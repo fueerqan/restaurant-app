0df89d81c772554b1c69cfc268e82bf79f42b768
Muhammad Furqan <furqan@tokopedia.com>
2022-07-24 14:38:49 +0700

    bug fixing

--- lib/ui/restaurant_detail_page.dart
+++ lib/ui/restaurant_detail_page.dart
@@ -1,8 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:restaurant_app/data/model/restaurant.dart';
 import 'package:restaurant_app/data/model/restaurant_menu.dart';
 import 'package:restaurant_app/data/restaurant_data_source.dart';
 import 'package:restaurant_app/widgets/icon_label.dart';
+import 'package:restaurant_app/widgets/platforms/platform_widget_builder.dart';
 import 'package:restaurant_app/widgets/restaurant_menu_item.dart' as RestoMenu;
 
 class RestaurantDetailPage extends StatelessWidget {
@@ -13,72 +15,90 @@ class RestaurantDetailPage extends StatelessWidget {
     // get restaurant id
     final restaurantId = ModalRoute.of(context)!.settings.arguments as String;
 
-    return Scaffold(
-      body: FutureBuilder(
-        future: RestaurantDataSource.fetchRestaurantById(context, restaurantId),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            if (snapshot.hasError) {
-              return Center(
+    return PlatformWidgetBuilder(
+      iOSBuilder: CupertinoPageScaffold(
+        child: _buildBody(context, restaurantId),
+      ),
+      androidBuilder: Scaffold(
+        body: _buildBody(context, restaurantId),
+      ),
+    );
+  }
+
+  FutureBuilder<Restaurant> _buildBody(
+      BuildContext context, String restaurantId) {
+    return FutureBuilder(
+      future: RestaurantDataSource.fetchRestaurantById(context, restaurantId),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          if (snapshot.hasError) {
+            return SizedBox(
+              width: double.infinity,
+              height: double.infinity,
+              child: Center(
                 child: Text(snapshot.error.toString()),
-              );
-            }
-
-            Restaurant restaurant = snapshot.data as Restaurant;
-
-            return Padding(
-              padding: const EdgeInsets.only(top: kTextTabBarHeight),
-              child: SingleChildScrollView(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    ClipRRect(
-                      borderRadius: const BorderRadius.only(
-                        bottomLeft: Radius.circular(10),
-                        bottomRight: Radius.circular(10),
-                      ),
-                      child: Image.network(restaurant.pictureId),
-                    ),
-                    _paddingWidget(
-                      Text(
-                        restaurant.name,
-                        style: Theme.of(context).textTheme.headline5,
-                      ),
-                      top: 16.0,
-                    ),
-                    _paddingWidget(
-                      IconLabel(
-                        icon: const Icon(
-                          Icons.location_on,
-                          color: Colors.grey,
-                        ),
-                        label: Text(
-                          restaurant.city,
-                          style:
-                              Theme.of(context).textTheme.bodyText1?.copyWith(
-                                    fontWeight: FontWeight.w300,
-                                    color: Colors.grey,
-                                    fontSize: 16,
-                                  ),
-                          overflow: TextOverflow.ellipsis,
-                          maxLines: 1,
-                        ),
-                      ),
-                      top: 8.0,
-                    ),
-                    ..._buildDescription(context, restaurant.desciption),
-                    ..._buildMenus(context, "Foods", restaurant.menus.foods),
-                    ..._buildMenus(context, "Drinks", restaurant.menus.drinks),
-                  ],
-                ),
               ),
             );
-          } else {
-            return const Expanded(
-                child: Center(child: CircularProgressIndicator()));
           }
-        },
-      ),
+
+          Restaurant restaurant = snapshot.data as Restaurant;
+
+          return Padding(
+            padding: const EdgeInsets.only(top: kTextTabBarHeight),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  ClipRRect(
+                    borderRadius: const BorderRadius.only(
+                      bottomLeft: Radius.circular(10),
+                      bottomRight: Radius.circular(10),
+                    ),
+                    child: Image.network(restaurant.pictureId),
+                  ),
+                  _paddingWidget(
+                    Text(
+                      restaurant.name,
+                      style: Theme.of(context).textTheme.headline5,
+                    ),
+                    top: 16.0,
+                  ),
+                  _paddingWidget(
+                    IconLabel(
+                      icon: const Icon(
+                        Icons.location_on,
+                        color: Colors.grey,
+                      ),
+                      label: Text(
+                        restaurant.city,
+                        style: Theme.of(context).textTheme.bodyText1?.copyWith(
+                              fontWeight: FontWeight.w300,
+                              color: Colors.grey,
+                              fontSize: 16,
+                            ),
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 1,
+                      ),
+                    ),
+                    top: 8.0,
+                  ),
+                  ..._buildDescription(context, restaurant.desciption),
+                  ..._buildMenus(context, "Foods", restaurant.menus.foods),
+                  ..._buildMenus(context, "Drinks", restaurant.menus.drinks),
+                ],
+              ),
+            ),
+          );
+        } else {
+          return const SizedBox(
+            width: double.infinity,
+            height: double.infinity,
+            child: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        }
+      },
     );
   }
 
--- lib/ui/restaurant_list_page.dart
+++ lib/ui/restaurant_list_page.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:restaurant_app/data/model/restaurant.dart';
 import 'package:restaurant_app/data/restaurant_data_source.dart';
+import 'package:restaurant_app/widgets/platforms/platform_widget_builder.dart';
 import 'package:restaurant_app/widgets/restaurant_item.dart';
 
 class RestaurantListPage extends StatelessWidget {
@@ -8,8 +10,13 @@ class RestaurantListPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: _buildBody(context),
+    return PlatformWidgetBuilder(
+      iOSBuilder: CupertinoPageScaffold(
+        child: _buildBody(context),
+      ),
+      androidBuilder: Scaffold(
+        body: _buildBody(context),
+      ),
     );
   }
 
